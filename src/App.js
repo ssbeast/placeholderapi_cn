88f0c5b978e3c4d8323e77d44b1d4b1e158c1b0b
ssbeast <ssbeast007@gmail.com>
2021-10-14 11:43:01 +0530

    Assesment Complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+class App extends React.Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            items: [],
+            DataisLoaded: false
+        };
+    }
+
+    componentDidMount() {
+        fetch(
+            "https://jsonplaceholder.typicode.com/photos?_start=0&_limit=50")
+            .then((res) => res.json())
+            .then((json) => {
+                this.setState({
+                    items: json,
+                    DataisLoaded: true
+                });
+            })
+    }
+    render() {
+        const { DataisLoaded, items } = this.state;
+        if (!DataisLoaded) return <div>
+            <h1> Pleses wait some time.... </h1> </div>;
+
+        return (
+            <div className="App">
+                <h1> Fetching data from placeholder API</h1>  {
+                    items.map((item) => (
+                        <div key={item.id} className="main">
+                            <h3>{item.title}</h3>
+                            <img src={item.url} alt="img" id="img"></img>
+                        </div>
+                    ))
+                }
+            </div>
+        );
+    }
 }
 
 export default App;
